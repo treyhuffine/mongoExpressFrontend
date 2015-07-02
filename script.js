8d5f4a6bcd597948958793819f29da936e147312
AgileLabs <samer@agilelabs.com>
2015-07-02 15:10:41 -0700

    Prep for question view

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 angular.module('heimdall', [])
 
-.controller('MainCtrl', function($scope){
+.controller('MainCtrl', function($scope, $http){
+  $http.get("http://localhost:3000/questions").success(function(data) {
+    $scope.questions = data;
+  }).error(function(err) {
+    console.log(err);
+  });
 });
