10f1fa751c7efd301362b228ba74e2320fe48c9c
Trey Huffine <treyhuffine@gmail.com>
2015-07-02 16:56:44 -0700

    Get and post questions

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-'use strict';
-
-angular.module('heimdall', [])
-
-.controller('MainCtrl', function($scope, $http){
-  $http.get("http://localhost:3000/questions").success(function(data) {
-    $scope.questions = data;
-  }).error(function(err) {
-    console.log(err);
-  });
+angular.module('askNerds', [])
+.constant("ATN", {
+  "API_URL": "http://localhost:3000"
+})
+.factory('Question', function($http, ATN) {
+  return {
+    getQuestions: function() {
+      return $http.get(ATN.API_URL + "/questions");
+    },
+    postQuestion: function(newQuestion) {
+      return $http.post(ATN.API_URL + "/questions", newQuestion);
+    }
+  };
+})
+.controller('MainCtrl', function($scope, $http, Question){
+  Question.getQuestions()
+    .success(function(data) {
+      $scope.questions = data;
+    }).catch(function(err) {
+      console.log(err);
+    });
+  $scope.postQuestion = function() {
+    Question.postQuestion($scope.question)
+      .success(function(data) {
+        $scope.questions.unshift(data);
+        $scope.question = {};
+        $("#new-question-modal").modal("hide");
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
 });
