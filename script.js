fae73dca3c77d1f47dde5f6be1101aae386f4067
Trey Huffine <treyhuffine@gmail.com>
2015-07-03 18:13:59 -0700

    Heavy rework

--- script.js
+++ script.js
@@ -1,55 +1,67 @@
-angular.module('askNerds', [])
+angular.module('questions', ['ui.router'])
 .constant("ATN", {
-  "API_URL": "http://localhost:3000"
+  "API_URL": "https://mongoexoress.herokuapp.com"
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "list.html",
+      controller: 'MainCtrl'
+    })
+    .state('question', {
+      url: "/:slug",
+      templateUrl: "question.html",
+      controller: "QuestionCtrl"
+    });
 })
 .factory('Question', function($http, ATN) {
   return {
-    getQuestions: function() {
+    getOne: function(slug) {
+      return $http.get(ATN.API_URL + "/questions/" + slug);
+    },
+    getAll: function() {
       return $http.get(ATN.API_URL + "/questions");
     },
-    postQuestion: function(newQuestion) {
+
+    addQuestion: function(newQuestion) {
       return $http.post(ATN.API_URL + "/questions", newQuestion);
-    },
-    viewQuestion: function(question) {
-      return $http.get(ATN.API_URL + "/question/" + question.slug);
     }
-  };
+  }
 })
-.controller('MainCtrl', function($scope, $http, $location, Question){
-  $scope.questions = {};
-  $scope.focusQuestion = {};
-  $scope.singleView = false;
-  Question.getQuestions()
+.filter("dateInWords", function() {
+  return function(input) {
+    return moment(input).utc().fromNow();
+  }
+})
+.controller('QuestionCtrl', function($scope, Question, $state){
+  $scope.slug = $state.params.slug;
+
+  Question.getOne($state.params.slug)
     .success(function(data) {
-      $scope.questions = data;
+      $scope.question = data;
     }).catch(function(err) {
-      console.log(err);
+      console.error(err);
     });
-  $scope.postQuestion = function() {
-    Question.postQuestion($scope.question)
+})
+.controller('MainCtrl', function($scope, Question){
+  Question.getAll().success(function(data) {
+    $scope.questions = data;
+  }).catch(function(err) {
+    console.error(err);
+  });
+
+  $scope.askQuestion = function() {
+    Question.addQuestion($scope.question)
       .success(function(data) {
         $scope.questions.unshift(data);
         $scope.question = {};
         $("#new-question-modal").modal("hide");
       })
-      .catch(function(error) {
-        console.error(error);
-      });
-  };
-  $scope.viewQuestion = function(question) {
-    Question.viewQuestion(question)
-      .success(function(data) {
-        $scope.focusQuestion = data;
-        $scope.singleView = true;
-        $location.url("/" + $scope.focusQuestion.slug);
+      .catch(function(err) {
+        console.error(err);
       })
-      .catch(function(error) {
-        console.log(error);
-      });
   };
-  $scope.backToMain = function() {
-    $scope.singleView = false;
-    $scope.focusQuestion = {};
-    $location.url("/");
-  };
+
 });
